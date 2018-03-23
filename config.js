02a88321c89960538a5a5bf3d101bf3b8d1276ff
subinhuang <806519191@qq.com>
2018-03-23 22:00:27 +0800

    beta1

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  cookieName: 'your_cookie_name',
+  mongodbUrl: 'mongodb://localhost:32772/firstapp',
+  // mongodbUrl: 'mongodb://firstapp:123456@ds121189.mlab.com:21189/nodejsfirst',
+
+}
