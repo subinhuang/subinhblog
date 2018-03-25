2207066a105f37d845f90a1fe32b5e4fa3ed793f
subinhuang <806519191@qq.com>
2018-03-25 11:39:13 +0800

    add gitignore1

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   cookieName: 'your_cookie_name',
   // mongodbUrl: 'mongodb://localhost:32772/firstapp',
-  mongodbUrl: 'mongodb://subin11:subinhuang@ds123499.mlab.com:23499/blog',
+  mongodbUrl: 'mongodb://subin11:subinhuang123@ds123499.mlab.com:23499/blog',
 }
