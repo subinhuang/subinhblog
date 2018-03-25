dfd41dde333b13bd3f8078898ddccc6087727cb6
subinhuang <806519191@qq.com>
2018-03-25 11:36:07 +0800

    add gitignore

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   cookieName: 'your_cookie_name',
   // mongodbUrl: 'mongodb://localhost:32772/firstapp',
-  mongodbUrl: 'mongodb://firstapp:123456@ds121189.mlab.com:21189/nodejsfirst',
-
+  mongodbUrl: 'mongodb://subin11:subinhuang@ds123499.mlab.com:23499/blog',
 }
