062bfa896a80810d38902a4e5f1e5a3f85dcb2cf
subinhuang <806519191@qq.com>
2018-04-08 20:02:33 +0800

    modify dburl

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   cookieName: 'your_cookie_name',
-  mongodbUrl: 'mongodb://localhost:32768/firstapp',
-  // mongodbUrl: 'mongodb://blog:123456@ds239029.mlab.com:39029/blog1',
+  // mongodbUrl: 'mongodb://localhost:32768/firstapp',
+  mongodbUrl: 'mongodb://blog:123456@ds239029.mlab.com:39029/blog1',
 
 }
